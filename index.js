b02888241a945fa2a0cc080c39fcaf2aacb8bda2
sahir khan <66465385+sahirskd@users.noreply.github.com>
2020-12-25 13:43:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function deleteNotes(index) {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("/sw.js")
+        .register("noteapp/sw.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
